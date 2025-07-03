16ca5f41800f19e669ffbcfeb8c0511fc3b74024
Akhtar Faizi Putra <akhtarerror@gmail.com>
2025-07-03 16:18:31 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:dart_ipify/dart_ipify.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String ipText = 'IP';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              ipText,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            TextButton(
+              onPressed: () async {
+                final ipv4 = await Ipify.ipv4();
+                setState(() {
+                  ipText = ipv4;
+                });
+              },
+              child: const Text('Get IP'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
